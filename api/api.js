60175c330ff53f502a92c55890f4b70179178f38
Brian Leathem <bleathem@gmail.com>
2013-10-28 22:09:42 -0700

    Updates

---
+++ api/api.js
@@ -0,0 +1,32 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "chart",
+        "orderingList",
+        "pickList",
+        "richAutocomplete"
+    ],
+    "modules": [
+        "Input",
+        "Output",
+        "Select"
+    ],
+    "allModules": [
+        {
+            "displayName": "Input",
+            "name": "Input",
+            "description": "An autocomplete widget"
+        },
+        {
+            "displayName": "Output",
+            "name": "Output",
+            "description": "A widget for creating data chart"
+        },
+        {
+            "displayName": "Select",
+            "name": "Select",
+            "description": "A widget providing sorting capabilities for a list of elements"
+        }
+    ]
+} };
+});
