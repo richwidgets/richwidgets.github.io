a58bc03369f366303a8d4b2ab6cec7a1a66d2992
Lukas Fryc <lfryc@redhat.com>
2013-11-04 17:41:09 +0100

    Updates

--- api/api.js
+++ api/api.js
@@ -1,10 +1,10 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "autocomplete",
         "chart",
         "orderingList",
-        "pickList",
-        "richAutocomplete"
+        "pickList"
     ],
     "modules": [
         "Input",
@@ -15,7 +15,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "Input",
             "name": "Input",
-            "description": "An autocomplete widget"
+            "description": "Autocomplete enables users to quickly find and select from a pre-populated list of values as they type, leveraging searching and filtering.\n\nThe widget is based on popular jQuery UI Autocomplete widget, but it brings some advanced built-in features:\n\n* caching\n* tokenizing\n* using DOM as a source of suggestions\n* auto-fill\n* built-in button"
         },
         {
             "displayName": "Output",
